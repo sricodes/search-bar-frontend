1b0ac5fc8aeac9f350759e4982fd8c3e38de1feb
sriram <sriram.kumar@whitehatjr.com>
2022-01-08 17:51:55 +0530

    compose yaml

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-
-
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand">Navbar</a>
+        <a class="navbar-brand">SRIRAM Navbar</a>
+        <div>
+          
+        </div>
         <form class="form-inline">
-          <input class="form-control mr-sm-20" type="search" placeholder="Search" aria-label="Search" style={{size: "100"}} />
+          <input class="form-control mr-sm-20" type="search" placeholder="Search" aria-label="Search" />
           <button class="btn btn-outline-success my-20 my-sm-20" type="submit">Search</button>
         </form>
       </nav>
